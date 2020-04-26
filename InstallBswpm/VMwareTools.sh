43ee0979897473386019cb4dbe566e9202de78fe
Loop-Man <60947094+Loop-Man@users.noreply.github.com>
2020-04-26 17:41:08 +0200

    Primer intento

---
+++ InstallBswpm/VMwareTools.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Introducir disco de las vmware tools"
+echo "Seleccionar en vmware VM --> Install VMware Tools Installation"
+echo "Pulsar cualquier tecla para continuar una vez realizado esto"
+input 
+sudo pacman -Syu
+sudo pacman -S --noconfirm --needed net-tools linux-headers xf86-input-vmmouse xf86-video-vmware
+sudo mkdir -p /etc/init.d/{rc0.d,rc1.d,rc2.d,rc3.d,rc4.d,rc5.d,rc6.d}
+sduo mkdir -p /mnt/cdrom
+sudo mount /dev/cdrom /mnt/cdrom
+sleep 5
+tar xf /mnt/cdrom/VMwareTools*.tar.gz -C ~/
+sudo perl ~/vmware-tools-distrib/vmware-install.pl
+sudo umount /dev/cdrom
+
