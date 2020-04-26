3b6386f1c5b8fff8eb956b591a5a03414d7af8d7
Loop-Man <60947094+Loop-Man@users.noreply.github.com>
2020-04-26 03:40:39 +0200

    Se añaden progrmas utiles y zsh

--- InstallBswpm/bspwmInstall.sh
+++ InstallBswpm/bspwmInstall.sh
@@ -152,7 +152,7 @@ sudo pacman -S --noconfirm --needed python python2
 
 echo "Installing category Graphics"
 
-sudo pacman -S --noconfirm --needed gimp
+#sudo pacman -S --noconfirm --needed gimp
 sudo pacman -S --noconfirm --needed eog
 
 echo "Installing category Internet"
@@ -281,7 +281,7 @@ yay -S --noconfirm --needed the_platinum_searcher-bin
 yay -S --noconfirm --needed ttf-font-awesome
 yay -S --noconfirm --needed ttf-mac-fonts
 yay -S --noconfirm --needed nerd-fonts-hack 
-yay -S --noconfirm --needed vivaldi
+#yay -S --noconfirm --needed vivaldi
 yay -S --noconfirm --needed oblogout
 yay -S --noconfirm --needed gksu
 
@@ -313,7 +313,7 @@ sudo pacman -S gtop --noconfirm --needed
 sudo pacman -S imagemagick --noconfirm --needed
 sudo pacman -S lxappearance-gtk3 --noconfirm --needed
 sudo pacman -S lxrandr --noconfirm --needed
-sudo pacman -S nitrogen --noconfirm --needed
+#sudo pacman -S nitrogen --noconfirm --needed
 sudo pacman -S playerctl --noconfirm --needed
 sudo pacman -S rofi --noconfirm --needed
 sudo pacman -S thunar --noconfirm --needed
@@ -415,3 +415,16 @@ sudo pacman -S --noconfirm --needed clamav
 
 # Criptography
 sudo pacman -S --noconfirm --needed gnupg
+
+# Custom Loop-Man
+
+sudo pacman -S --noconfirm --needed code        # Para programar con visual-studio-code
+sudo pacman -S --noconfirm --needed meld        # Para comparar ficheros o carpetas
+sudo pacman -S --noconfirm --needed xorg-xkill  # Para matar graficamente un programa
+sudo pacman -S --noconfirm --needed praght      # Para reproducir audios
+
+# Start with zsh
+sudo pacman -S --noconfirm --needed lsd         # ls con vitaminas
+sudo pacman -S --noconfirm --needed bat         # cat con vitaminas
+sudo pacman -S --noconfirm --needed fzf         # buscador cli basado en bash o zsh history
+sudo pacman -S --noconfirm --needed zsh zsh-doc zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-lovers zsh-syntax-highlighting zshdb zsh-theme-powerlevel10k
