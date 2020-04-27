83c65a98f0b1a8cd796a9ea1014b9f08c6e159c0
Loop-Man <60947094+Loop-Man@users.noreply.github.com>
2020-04-27 21:30:30 +0200

    Añadido fzf de github

--- InstallBswpm/InstallZSH.sh
+++ InstallBswpm/InstallZSH.sh
@@ -10,11 +10,11 @@ sudo pacman -S --noconfirm --needed zsh-history-substring-search
 sudo pacman -S --noconfirm --needed zsh-lovers
 sudo pacman -S --noconfirm --needed zsh-syntax-highlighting
 sudo pacman -S --noconfirm --needed zshdb
-sudo pacman -S --noconfirm --needed zsh-theme-powerlevel10k
+#sudo pacman -S --noconfirm --needed zsh-theme-powerlevel10k
 
 sudo pacman -S --noconfirm --needed lsd
 sudo pacman -S --noconfirm --needed bat
-sudo pacman -S --noconfirm --needed fzf
+#sudo pacman -S --noconfirm --needed fzf
 
 ##### Instalamos zsh #######
 
@@ -26,6 +26,9 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.zsh/.oh-
 sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k/1' .zshrc
 rm -rf .zshrc.tmp
 rm -rf install.sh
+
+## Instalo fzf:
+git clone --depth 1 https://github.com/junegunn/fzf.git  ~/.fzf
 
 cp .zshrc ~/
 cp -r .zsh/ ~/
